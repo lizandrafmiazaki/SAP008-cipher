fb4e06226c952ab197a861885ea6b2dbb400bc0b
lizandrafmiazaki <lizandramiazaki@gmail.com>
2022-07-26 16:39:06 -0300

    projeto Cipher 1º commit, sem os testes, mas aparentemente tudo ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
-const cipher = {
-  // ...
-};
+const cipher = { encode:
 
+function encode (text, shift) {
+  let converterTexto = "";
+  for (let index = 0; index < text.length; index++) {
+    console.log(text.charCodeAt(index) - 65 + shift);
+
+    let cifrar = ((text.charCodeAt(index) - 65 + shift) % 26) + 65;
+    console.log(cifrar);
+    converterTexto = converterTexto + String.fromCharCode(cifrar);
+  }
+  console.log(converterTexto);
+  return converterTexto;
+},
+decode: 
+function decode (text, shift) {
+  let desconverterTexto = "";
+  for (let index = 0; index < text.length; index++) {
+    let decifrar = (((((text.charCodeAt(index) - 65) - shift) + 26) % 26) + 65);
+    console.log(decifrar);
+    desconverterTexto = desconverterTexto + String.fromCharCode(decifrar);
+  }
+  console.log(desconverterTexto);
+  return desconverterTexto;
+}
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import cipher from './cipher.js';
 
+const cifrar = document.getElementById("cifrar");
+cifrar.addEventListener("click", clickCifrar);
+
+function clickCifrar() {
+    let mensagem = document.getElementById("mensagem").value; 
+    let numDesloc = parseInt (document.getElementById("offset").value);
+    console.log(typeof numDesloc);
+
+    if (!mensagem) {
+        window.alert("Por favor, digite sua mensagem!");
+    }
+
+    else {
+        let crip = cipher.encode(mensagem, numDesloc);
+        let converterTexto = document.getElementById("convertexto");
+        converterTexto.innerHTML = crip;
+
+    }
+};
+
+
+const decifrar = document.getElementById("decifrar");
+decifrar.addEventListener("click", clickDecifrar);
+
+function clickDecifrar () {
+    let mensagem = document.getElementById("mensagem").value; 
+    let numDesloc = parseInt(document.getElementById("offset").value);
+
+    if (!mensagem) {
+        window.alert("Por favor, digite sua mensagem!");
+    }
+
+    else {
+        let descrip = cipher.decode(mensagem, numDesloc);
+        let desconverterTexto = document.getElementById("convertexto");
+        desconverterTexto.innerHTML = descrip;
+
+    }
+};
+
+
 console.log(cipher);
+
